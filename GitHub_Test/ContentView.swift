bec50755f43e0e9520d9d0b96359795c4d4aa9b5
bradycox <bcc17b@acu.edu>
2020-10-14 22:26:13 -0500

    Initial Commit

---
+++ GitHub_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHub_Test
+//
+//  Created by Brady Cox on 10/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub_Test/GitHub_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TestApp.swift
+//  GitHub_Test
+//
+//  Created by Brady Cox on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
